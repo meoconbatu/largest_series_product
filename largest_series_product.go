c3eaf2a04fd6b89990c84ae04a3b6f21f5e8e10e
Thanh Mi Nguyen Thanh <thanhmi1111@live.com>
2017-04-14 20:42:04 +0700

    optimize: use 2 threads

--- largest_series_product.go
+++ largest_series_product.go
@@ -15,18 +15,46 @@ func LargestSeriesProduct(digits string, span int) (product int64, err error) {
 		err = errors.New("error")
 		return
 	}
-	for i := 0; i+span <= digitsLen; i++ {
+	productChan := make(chan int64, digitsLen)
+	errChan := make(chan error, 2)
+	temp := int(digitsLen / 2)
+	go Worker(productChan, errChan, digits, span, 0, temp)
+	go Worker(productChan, errChan, digits, span, temp+1-span, digitsLen)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case err = <-errChan:
+			return
+		case tempProduct := <-productChan:
+			if tempProduct > product {
+				product = tempProduct
+			}
+		}
+	}
+	return
+}
+func Worker(productChan chan int64, errChan chan error, digits string, span int, from int, to int) {
+	product := int64(-1)
+	digitsLen := len(digits)
+
+	if span < 0 || span > digitsLen || from < 0 || to < 0 {
+		productChan <- product
+		errChan <- errors.New("error")
+		return
+	}
+	for i := from; i+span <= to; i++ {
 		seriesAtIndex := digits[i : i+span]
 		tempProduct, tempErr := Multiple(seriesAtIndex)
 		if tempErr != nil {
-			err = tempErr
+			errChan <- errors.New("error")
+			productChan <- product
 			return
 		}
 		if tempProduct > product {
 			product = tempProduct
 		}
 	}
-	return
+	productChan <- product
 }
 func Multiple(series string) (output int64, err error) {
 	output = 1
